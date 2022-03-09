31b7a25515a69e1d015eebc2b5a4c6eed0d5480c
lucaperini <luca.perini@gmail.com>
2022-03-09 16:05:51 +0100

    milestone 1

--- js/main.js
+++ js/main.js
@@ -1,32 +1,43 @@
 const team = [
     {
-      name: 'Wayne Barnett',
-      role: 'Founder & CEO',
-      image: 'wayne-barnett-founder-ceo.jpg',
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
-      name: 'Angela Caroll',
-      role: 'Chief Editor',
-      image: 'angela-caroll-chief-editor.jpg',
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
     },
     {
-      name: 'Walter Gordon',
-      role: 'Office Manager',
-      image: 'walter-gordon-office-manager.jpg',
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
     },
     {
-      name: 'Angela Lopez',
-      role: 'Social Media Manager',
-      image: 'angela-lopez-social-media-manager.jpg',
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
     },
     {
-      name: 'Scott Estrada',
-      role: 'Developer',
-      image: 'scott-estrada-developer.jpg',
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
     },
     {
-      name: 'Barbara Ramos',
-      role: 'Graphic Designer',
-      image: 'barbara-ramos-graphic-designer.jpg',
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+for (let name in team){
+    console.log(name, ":", team[name]);
+}
+for (let role in team){
+    console.log(role, ":", team[role]);
+}
+
+for (let image in team){
+    console.log(image, ":", team[image]);
+}
